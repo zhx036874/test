24ba640434fd5a8333360a3d9589c0a96691a8f5
zhx036874 <878830815@qq.com>
2024-09-05 22:30:01 +0800

    feat:1 Upgrade the log to an independent log file and no longer  in themain file.      2 modified CMake to allow it to traverse files in the src directory and automatically find .cpp and .h files in the src directory.

--- src/DebugHelper.h
+++ src/DebugHelper.h
@@ -1,7 +1,8 @@
-#include "widget.h"
+#ifndef DEBUGHELPER_H
+#define DEBUGHELPER_H
 
-#include <QApplication>
 #include <QDebug>
+#include <QString>
 
 // 自定义 DebugHelper 类，支持流式写法
 class DebugHelper
@@ -10,37 +11,35 @@ public:
     // 构造函数，传入文件、行号、函数信息
     DebugHelper(const char* file, int line, const char* function)
     {
-        // 拼接文件、函数、行号信息为一个字符串
         buffer = QString("[QDebug] File: %1 | Function: %2 | Line: %3 | ")
                      .arg(file)
                      .arg(function)
                      .arg(line);
     }
-
-    // 重载 << 操作符，用于打印任意类型的参数
+     //，如果继续使用模板函数，所有模板相关的实现都应该放在头文件中，以避免链接错误。
+    // 重载 << 操作符，用于处理模板类型
     template<typename T>
     DebugHelper& operator<<(const T& value)
     {
-        // 将非 QString 类型的值转换为 QString 并追加到 buffer
-        buffer += QString::number(value);
-        return *this; // 返回自身以支持链式调用
+        buffer += QString::number(value);  // 处理数值类型
+        return *this;
     }
 
     // 重载 << 操作符，用于处理 QString 类型
     DebugHelper& operator<<(const QString& value)
     {
-        buffer += value; // 直接拼接 QString 类型
+        buffer += value;
         return *this;
     }
 
     // 重载 << 操作符，用于处理 const char* 类型
     DebugHelper& operator<<(const char* value)
     {
-        buffer += QString(value); // 将 const char* 转换为 QString
+        buffer += QString(value);
         return *this;
     }
 
-    // 析构函数，最终输出所有内容
+    // 析构函数，输出最终的调试信息
     ~DebugHelper()
     {
         qDebug().noquote() << buffer;
@@ -53,24 +52,4 @@ private:
 // 宏简化调用，传递文件名、行号和函数名
 #define DEBUG DebugHelper(__FILE__, __LINE__, __FUNCTION__)
 
-void  testDebugcalssFunction()
-{
-     DEBUG <<"test the classfunction";
-}
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Widget w;//测试下上传项目
-    w.show();
-
-
-    int userId = 123;
-    QString userName = "John Doe";
-    double userScore = 99.5;
-
-    // Use custom DEBUG stream-style writing
-    DEBUG << "User ID:" << userId << " Name:" << userName << " Score:" << userScore ;
-    testDebugcalssFunction();
-    return a.exec();
-}
+#endif // DEBUGHELPER_H
